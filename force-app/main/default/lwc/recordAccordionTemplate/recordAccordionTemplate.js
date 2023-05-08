bb1f1a7fe6967e6614eebebaf211c24b56cb8781
LAPTOP-VS9ASC0I\Nitesh Kumawat <nitesh.kumawat@thinkvibes.com>
2023-05-08 15:05:48 +0530

    code review changes

--- force-app/main/default/lwc/recordAccordionTemplate/recordAccordionTemplate.js
+++ force-app/main/default/lwc/recordAccordionTemplate/recordAccordionTemplate.js
@@ -8,4 +8,17 @@ export default class RecordAccordionTemplate extends LightningElement {
     @api detailFieldSet
     @api objectName
     @api title
+
+    @api column
+    get isColOne() {
+        return (this.column == 1) ? true : false;
+    }
+
+    get isColTwo() {
+        return (this.column == 2) ? true : false;
+    }
+
+    get isColThree() {
+        return (this.column == 3) ? true : false;
+    }
 }
